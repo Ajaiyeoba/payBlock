208d2fe72eb17762f76ce52e10ca245f7a365d32
Ajaiyeoba <ajaiyeobajibola@gmail.com>
2024-01-20 01:51:34 -0800

    issues with the mobile nav

--- payblock/src/components/Hero.jsx
+++ payblock/src/components/Hero.jsx
@@ -3,8 +3,8 @@ import {Button} from './Button'
 function Hero() {
   return (
     <div className='hero-container'>
-        <h1>Click to Receive Payment</h1>
-        <p>Beg Smoothly</p>
+        <h1>Click to Receive Payment Send a Personalized Heartfelt Message</h1>
+        <p>Beg Smoothly! </p>g
         <div className="hero-btn">
             <Button className='btn' buttonStyle='btn-outline'>
                 Receive
--- payblock/src/components/Navbar.jsx
+++ payblock/src/components/Navbar.jsx
@@ -1,54 +1,59 @@
 import React, { useState, useEffect } from 'react'
 import {Link} from 'react-router-dom'
-// import { Button } from './Button'
+ import { Button } from './Button'
 import './Navbar.css'
 
 
 function Navbar() {
     const [click, setClick] = useState(false);
-   // const [button, setButton] =useState(true);
+   const [button, setButton] =useState(true);
 
    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false)
+   const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false); 
+        } else{
+            setButton(true);
+        }
+    };
 
-   // const showButton = () => {
-    //     if(window.innerWidth <= 960) {
-    //         setButton(false); 
-    //     } else{
-    //         setButton(true);
-    //     }
-    // };
+    useEffect(() => {
+        showButton(); 
+    }, []);
 
-    // window.addEventListener('resize', showButton)
+    window.addEventListener('resize', showButton)
 
   return (
     <> 
         <nav className="navbar">
         <div className="navbar-container">
-    <Link to='/' className='navbar-logo'> payBlock</Link>
-  
-        <div className="menu-icon" onClick={handleClick}>
+    <Link to='/' className='navbar-logo'> <i className="fa-solid fa-money-bill" /> pay <span>Block</span></Link>
 
+        <div className="menu-icon" onClick={handleClick}>
+        <i className={click ? 'fa-solid fa-x' : 'fa-solid fa-bars' }/>
+        
         </div>
 
         <ul className={click ? 'nav-menu acive': 'nav-menu'}>
             <li className='nav-item'>
-                <Link to='/' className='nav-links'>Home</Link>
+                <Link to='/' className='nav-links' onClick={closeMobileMenu}>Home</Link>
             </li>
             <li className='nav-item'>
-                <Link to='/products' className='nav-links'>Products</Link>
+                <Link to='/products' className='nav-links' onClick={closeMobileMenu}>Products</Link>
             </li>
             <li className='nav-item'>
-                <Link to='/contact' className='nav-links'>Contact</Link>
+                <Link to='/contact' className='nav-links' onClick={closeMobileMenu}>Contact</Link>
             </li>
             <li className='nav-item'>
-                <Link to='/services' className='nav-links'>Services</Link>
+                <Link to='/services' className='nav-links' onClick={closeMobileMenu}>Services</Link>
             </li>
             <li className='nav-item'>
-                <Link to='/sign-up' className='nav-links-mobile'>Sign up</Link>
+                <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>Sign up</Link>
             </li>
         </ul>
 
-        {/* {button && <Button buttonStyle='btn-outline'> Sign Up </Button>} */}
+        {button && <Button buttonStyle='btn btn-outline'> Sign Up </Button>}
         </div>
     </nav>
     </>
