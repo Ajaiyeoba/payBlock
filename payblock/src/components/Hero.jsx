fe25f34e0e004f3a498c8f753c03f710295bb706
Ajaiyeoba <ajaiyeobajibola@gmail.com>
2024-01-24 09:25:33 -0800

    decided to make the hero section one with an height of 50vh

--- payblock/src/components/Hero.jsx
+++ payblock/src/components/Hero.jsx
@@ -7,9 +7,12 @@ function Hero() {
   return (
     <div className='hero-container'>
         <h1> <span>Personalized </span>  Heartfelt Message</h1>
-        <p>Beg Smoothly! Click to Receive Payment. Seeking Financial Help can be horrible at times     </p>
+        <p>Beg Smoothly! Click to Receive Payment. Seeking Financial Help can be horrible at times  Rizz them with your messages
+              </p>
+       
         <div className="hero-btn">
-            <Button className='btn' buttonStyle='btn-outline'>
+
+        <Button className='btn' buttonStyle='btn-primary'>
              Receive Payment </Button>
         </div>
     </div>
