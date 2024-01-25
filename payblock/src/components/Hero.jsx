caaecc59a92d7669e7aee046f65011b4e18a224a
Ajaiyeoba <ajaiyeobajibola@gmail.com>
2024-01-25 11:32:42 -0800

    this little project is making me understand media query and responsiveness

--- payblock/src/components/Hero.jsx
+++ payblock/src/components/Hero.jsx
@@ -6,16 +6,20 @@ import './Hero.css'
 function Hero() {
   return (
     <div className='hero-container'>
-        <h1> <span>Personalized </span>  Heartfelt Message</h1>
-        <p>Beg Smoothly! Click to Receive Payment. Seeking Financial Help can be horrible at times  Rizz them with your messages
-              </p>
-       
+        <h1><span>Personalized</span> Heartfelt Message</h1>
+        <p>Beg Smoothly! Click to Receive Payment.
+           Seeking Financial Help can be horrible at times. Rizz them with your style! They should not ask for your details </p>
         <div className="hero-btn">
-
-        <Button className='btn' buttonStyle='btn-primary'>
+        <Button className='btn' buttonStyle='btn-outline'>
              Receive Payment </Button>
         </div>
+
+
     </div>
+
+
+
+
   )
 }
 
--- payblock/src/components/pages/Home.jsx
+++ payblock/src/components/pages/Home.jsx
@@ -4,6 +4,7 @@ import '../../App.css'
 function Home() {
     return (
         <>
+        
         <Hero />
         
         </>
